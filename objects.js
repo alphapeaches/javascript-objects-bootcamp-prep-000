737c30ff83ac0bdbfd6aaa843a87ae636272abc4
Rachel Acosta <denversunrise@gmail.com>
2017-12-13 14:36:45 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,9 +1,9 @@
 var playlist = {'Ed Sheeran': "Thinking Out Loud"};
 
-//function updatePlaylist(playlist, artistName, songTitle){
- // playlist.[artistName] = [songTitle];
-  //return playlist;
-//}
+function updatePlaylist(playlist, artistName, songTitle){
+ [playlist].[artistName] = [songTitle];
+  return playlist;
+}
 
 //function removeFromPlaylist(playlist, artistName, songTitle){
   //delete playlist.artistName;
